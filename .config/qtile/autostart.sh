37fbf9d899c94b0b4d6081bd4e66fac6868f2c4c
Nacho-17-ghost <ignaciosepulveda2117@gmail.com>
2021-07-20 04:10:48 -0400

    configuracion 1

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# systray battery icon
+#cbatticon -u 5 &
+
+# systray volume
+#volumeicon &
+
+# wallpaper
+#feh --bg-scale Imágenes/ghost.jpg
+
+# keyboard
+#setxkbmap latam
+
+# picom
+#picom &
+
+# usb
+#udiskie -t &
+
+# wifi
+#nm-applet &
+
